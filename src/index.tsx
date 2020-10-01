676038260d6ffc9c49b57b07f14878a333d5d15d
layrodrgs <layrodrgs@gmail.com>
2020-10-01 11:06:43 -0300

    criando o projeto

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DashBoard from './pages/DashBoard';
+import App from './pages/DashBoard';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DashBoard />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
